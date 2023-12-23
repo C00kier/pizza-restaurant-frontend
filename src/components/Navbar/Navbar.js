bb0430700d729bf96bb70b8060a12eacec0c8cec
Pawel Ignaczak <ignak265@gmail.com>
2023-12-23 15:40:29 +0100

    Feature/ added navbar version for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 //assets
 import userIcon from "../../assets/navbar/userIcon.png";
@@ -10,35 +11,58 @@ import blankIcon from "../../assets/common/blankIcon.png";
 import PAGES from "../../constants/PAGES";
 
 export default function Navbar() {
-const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-function redirectToHomePage(){
-    navigate(PAGES.HOME);
-}
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
 
-function redirectToLoginPage(){
-    navigate(PAGES.LOGIN);
-}
+        window.addEventListener('resize', handleWindowResize);
 
-function redirectToOfferPage(){
-    navigate(PAGES.OFFER);
-}
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    function redirectToHomePage() {
+        navigate(PAGES.HOME);
+    }
+
+    function redirectToLoginPage() {
+        navigate(PAGES.LOGIN);
+    }
+
+    function redirectToOfferPage() {
+        navigate(PAGES.OFFER);
+    }
 
     return (
         <div className="navbar-grid">
             <div className="flex align-center navbar-left-section">
-                <img className="navbar-logo" src={blankIcon} alt="logo" onClick={() => redirectToHomePage()}/>
+                <img className="navbar-logo" src={blankIcon} alt="logo" onClick={() => redirectToHomePage()} />
                 <p className="navbar-logo-text" onClick={() => redirectToHomePage()}>Cookier's Pizza</p>
             </div>
             <div className="flex justify-end align-center navbar-right-section">
-                <div className="flex navbar-element">
-                    <p className="navbar-element-text" onClick={() => redirectToLoginPage()}>Zaloguj się</p>
-                    <img className="navbar-icon" src={userIcon} alt="user" onClick={() => redirectToLoginPage()}/>
-                </div>
-                <div className="flex navbar-element">
-                    <p className="navbar-element-text" onClick={() => redirectToOfferPage()}>Koszyk</p>
-                    <img className="navbar-icon" src={cartIcon} alt="cart" onClick={() => redirectToOfferPage()}/>
-                </div>
+                <>
+                    <div className="flex navbar-element">
+                        {
+                            windowWidth > 767
+                            &&
+                            <p className="navbar-element-text" onClick={() => redirectToLoginPage()}>Zaloguj się</p>
+                        }
+                        <img className="navbar-icon" src={userIcon} alt="user" onClick={() => redirectToLoginPage()} />
+                    </div>
+                    <div className="flex navbar-element">
+                        {
+                            windowWidth > 767
+                            &&
+                            <p className="navbar-element-text" onClick={() => redirectToOfferPage()}>Koszyk</p>
+                        }
+                        <img className="navbar-icon" src={cartIcon} alt="cart" onClick={() => redirectToOfferPage()} />
+                    </div>
+                </>
             </div>
         </div>
     )
